7ffff3609d05145717f33c57a7fc3bc7d18d18fa
Shahboz <sharafzodashahboz@gmail.com>
2024-03-11 18:21:51 +0500

    url for two views handled

--- job/urls.py
+++ job/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path 
+from django.urls import path, include
+from rest_framework import routers
+from . import views
 
-from . import views 
+
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('location/', views.LocationListCreate.as_view()),
+    path('location/<int:pk>/', views.LocationEdit.as_view()),
 ]
