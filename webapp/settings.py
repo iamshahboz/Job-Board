a752130215d05075c6a98cc13d8f7130999cc39b
Shahboz <sharafzodashahboz@gmail.com>
2024-03-11 18:20:09 +0500

    secret key handled

--- webapp/settings.py
+++ webapp/settings.py
@@ -1,6 +1,10 @@
-
-
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9jsau)h0-_&y%z1_zv4092aymixzw3p%&eclsu^*&p3lov&r6b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'job',
     'account',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
