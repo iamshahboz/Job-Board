13b86113e3a58f7c156d312ab9093c516752beaa
Shahboz <sharafzodashahboz@gmail.com>
2024-03-11 18:22:37 +0500

    app routes included

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('job/', include('job.urls')),
+    path('account/',include('account.urls')),
     path('admin/', admin.site.urls),
 ]
