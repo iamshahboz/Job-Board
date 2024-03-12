d08edacb13f4b9a74f697fb3d4813c71d101ea54
Shahboz <sharafzodashahboz@gmail.com>
2024-03-12 06:54:59 +0500

    candidate, university, skill views

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .serializers import CandidateSerializer, UniversitySerializer
+from .models import Candidate, University
+from rest_framework import generics
 
-# Create your views here.
+# views for each model created
+# order is -id and it means that the last created item comes first
+
+class CandidateListCreate(generics.ListCreateAPIView):
+    queryset = Candidate.objects.all().order_by('-id')
+    serializer_class = CandidateSerializer
+
+class CandidateEdit(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Candidate.objects.all().order_by('-id')
+    serializer_class = CandidateSerializer
+
+class UniversityListCreate(generics.ListCreateAPIView):
+    queryset = University.objects.all().order_by('-id')
+    serializer_class = UniversitySerializer
+
+class UniversityEdit(generics.RetrieveUpdateDestroyAPIView):
+    queryset = University.objects.all().order_by('-id')
+    serializer_class = UniversitySerializer
+
+    
