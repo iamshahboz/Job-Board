c409d42e66b8733670f25dfcb6cb68c5b14fa719
Shahboz <sharafzodashahboz@gmail.com>
2024-03-12 06:54:21 +0500

    url for models defined

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     #path('', views.homepage, name='homepage'),
+    path('candidate/', views.CandidateListCreate.as_view()),
+    path('candidate/<int:pk>/',views.CandidateEdit.as_view()),
+    path('university/',views.UniversityListCreate.as_view()),
+    path('university/<int:pk>/',views.UniversityEdit.as_view()),
 ]
