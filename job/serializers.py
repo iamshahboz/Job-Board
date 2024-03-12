9b3f0d26354b1730ba168c26209e57d97a59888b
Shahboz <sharafzodashahboz@gmail.com>
2024-03-12 06:57:12 +0500

    id added

--- job/serializers.py
+++ job/serializers.py
@@ -2,24 +2,29 @@ from .models import Location, Skill, Vacancy, Company
 from rest_framework import serializers 
 
 class LocationSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Location 
-        fields = ['city']
+        fields = ['id','city']
 
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
+        id = serializers.IntegerField(read_only=True)
         model = Skill 
-        fields = ['skill_name']
+        fields = ['id','skill_name']
 
 class VacancySerializer(serializers.ModelSerializer):
     class Meta:
+        id = serializers.IntegerField(read_only=True)
+        skills  = serializers.PrimaryKeyRelatedField(queryset=Skill.objects.all(),many=True)
         model = Vacancy
-        fields = ['title','description','salary','location','company','is_active','skills']
+        fields = ['id','title','description','salary','location','company','is_active','skills']
 
 class CompanySerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Company 
-        fields = ['name','description','logo','verified']
+        fields = ['id','name','description','logo','verified']
         
 
