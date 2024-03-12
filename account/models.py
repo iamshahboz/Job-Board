769d2b3bd42a25db88f0a532307c7df76d7bdc32
Shahboz <sharafzodashahboz@gmail.com>
2024-03-12 06:48:23 +0500

    models got changed

--- account/models.py
+++ account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from job.models import Skill
 
+# in order to add created_at, updated_at fields to all models this model created
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -19,13 +20,13 @@ class Candidate(TimeStampedModel):
     name = models.CharField(max_length=100)
     date_of_birth = models.DateField()
     photo = models.ImageField()
-    education = models.ForeignKey(University, on_delete=models.CASCADE, blank=True, null=True)
+    education = models.ManyToManyField(University, related_name='candidates_attended')
     class DegreeChoices(models.TextChoices):
         BACHELORS = "B.S.", ('Bachelors')
         MASTERS = "M.S.", ('Masters')
         DOCTOR = "Ph.D.", ('Ph.D.')
     degree = models.CharField(max_length=20, choices=DegreeChoices.choices)
-    skill = models.ManyToManyField(Skill, related_name='candidates')
+    skill = models.ManyToManyField(Skill, related_name='candidates_with_skill')
 
     def __str__(self):
         return self.name 
