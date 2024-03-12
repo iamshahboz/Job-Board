81dc0f7972011154cc4fe63d9ac38e5e4ac9756d
Shahboz <sharafzodashahboz@gmail.com>
2024-03-12 06:58:05 +0500

    url added for Vacancy, Company

--- job/urls.py
+++ job/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('location/', views.LocationListCreate.as_view()),
     path('location/<int:pk>/', views.LocationEdit.as_view()),
+    path('skill/',views.SkillListCreate.as_view()),
+    path('skill/<int:pk>/',views.SkillEdit.as_view()),
+    path('vacancy/',views.VacancyListCreate.as_view()),
+    path('vacancy/<int:pk>/',views.VacancyEdit.as_view()),
+    path('company/',views.CompanyListCreate.as_view()),
+    path('company/<int:pk>/',views.CompanyEdit.as_view()),
+    
 ]
