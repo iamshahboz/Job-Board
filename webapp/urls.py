154c0fb07d29ead695b1f5b41115da0429c2cbd1
Shahboz <sharafzodashahboz@gmail.com>
2024-03-20 23:57:09 +0500

    token obtain url

--- webapp/urls.py
+++ webapp/urls.py
@@ -8,10 +8,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+
 urlpatterns = [
     path('job/', include('job.urls')),
     path('account/',include('account.urls')),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
