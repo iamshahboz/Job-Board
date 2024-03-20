b7ed1f655e81aaf00d5eaf0a5e91f51a6ea332ec
Shahboz <sharafzodashahboz@gmail.com>
2024-03-20 23:56:02 +0500

    small change

--- account/urls.py
+++ account/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 from . import views 
 
 urlpatterns = [
     #path('', views.homepage, name='homepage'),
+
     path('candidate/', views.CandidateListCreate.as_view()),
     path('candidate/<int:pk>/',views.CandidateEdit.as_view()),
     path('university/',views.UniversityListCreate.as_view()),
     path('university/<int:pk>/',views.UniversityEdit.as_view()),
+
 ] 
