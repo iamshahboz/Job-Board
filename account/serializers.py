2c5a3698459e83781ba99da7a7d0cdff712b7a22
Shahboz <sharafzodashahboz@gmail.com>
2024-03-26 03:10:23 +0500

    creating candidate handled

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers 
 from .models import User, University, Skill
 from job.serializers import SkillSerializer
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
 
 # since skill has ManyToManyField in the database level
@@ -12,16 +14,44 @@ class UniversitySerializer(serializers.ModelSerializer):
         model = University
         fields = ['id','name']
 
+        
+
 class CandidateSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    education = serializers.PrimaryKeyRelatedField(queryset=University.objects.all(),many=True)
-    skill = serializers.PrimaryKeyRelatedField(queryset=Skill.objects.all(),many=True)
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
     
 
-
     class Meta:
-        model = User 
-        fields = ['id','surname','name','date_of_birth','photo','education','degree','skill','phone']
+        model = User
+        fields = ['id', 'surname', 'name', 'email', 'password', 'password2']
+
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            surname = validated_data['surname'],  
+            name = validated_data['name'],   
+            email=validated_data['email'],
+            role = User.RoleChoices.CANDIDATE
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user 
+    
+        
+
+    
+
 
 
 
