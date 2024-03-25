1d69206aa887d8a113338a051e91a029791df813
Shahboz <sharafzodashahboz@gmail.com>
2024-03-26 03:44:23 +0500

    view for partial update

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 
-from .serializers import CandidateSerializer, UniversitySerializer
+from .serializers import CandidateSerializer, UniversitySerializer,CandidateEdit
 from .models import User, University
 from rest_framework import generics
 from django.db import transaction
@@ -11,7 +11,7 @@ from rest_framework import status
 # views for each model created
 # order is -id and it means that the last created item comes first
 
-class CandidateListCreate(generics.CreateAPIView):
+class CandidateListCreate(generics.ListCreateAPIView):
     queryset = User.objects.all().order_by('-id')
     serializer_class = CandidateSerializer
 
@@ -20,7 +20,14 @@ class CandidateListCreate(generics.CreateAPIView):
 
 class CandidateEdit(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all().order_by('-id')
-    serializer_class = CandidateSerializer
+    serializer_class = CandidateEdit
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response
 
 class UniversityListCreate(generics.ListCreateAPIView):
     queryset = University.objects.all().order_by('-id')
