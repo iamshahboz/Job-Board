7fd599ed91124d60a346b1c3d750584bc5d6c958
Shahboz <sharafzodashahboz@gmail.com>
2024-03-26 03:10:58 +0500

    some imports

--- account/views.py
+++ account/views.py
@@ -2,15 +2,21 @@
 from .serializers import CandidateSerializer, UniversitySerializer
 from .models import User, University
 from rest_framework import generics
+from django.db import transaction
+from rest_framework.response import Response
+from rest_framework import status
 
 
 
 # views for each model created
 # order is -id and it means that the last created item comes first
 
-class CandidateListCreate(generics.ListCreateAPIView):
+class CandidateListCreate(generics.CreateAPIView):
     queryset = User.objects.all().order_by('-id')
     serializer_class = CandidateSerializer
+
+
+
 
 class CandidateEdit(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all().order_by('-id')
