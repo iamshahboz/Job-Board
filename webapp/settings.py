5f520e562baa0767ccb5919418907ad103f31ea1
Shahboz <sharafzodashahboz@gmail.com>
2024-03-26 05:41:01 +0500

    setting added for test case

--- webapp/settings.py
+++ webapp/settings.py
@@ -85,7 +85,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
     
 }
 
